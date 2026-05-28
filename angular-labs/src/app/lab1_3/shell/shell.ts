33d09e7a1793423319023d2eebd28833e1a0049f
VinhHV28 <vinhhv28@gmail.com>
2026-05-28 08:35:31 +0700

    chore(lab1.3: Add explain comment for standalone component)

--- angular-labs/src/app/lab1_3/shell/shell.ts
+++ angular-labs/src/app/lab1_3/shell/shell.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+
+// Đường dẫn đến 2 components
 import { Navbar } from '../navbar/navbar';
 import { Sidebar } from '../sidebar/sidebar';
+
+/**
+ * Khi Angular biên dịch một Standalone Component (vd: ShellComponent),
+ * quá trình tìm kiếm diễn ra:
+ * 1. Quét file Template HTML để tìm thẻ Selector
+ * --> Angular Compiler sẽ đọc file HTML (shell.component.html) và
+ *  phát hiện ra các Custom Tags không phải của HTML chuẩn, ví dụ: <app-navbar> và <app-sidebar>.
+ *
+ * 2. Đối chiếu trực tiếp vào mảng imports của @Component
+ * --> Nếu NgModule sẽ phải tìm ở file app.ts ở xa
+ * --> Angular Compiler tìm trong shell.ts
+ *
+ * 3.Tìm file vật lý dựa vào import của TypeScript:
+ * --> sau khi tìm được Navbar trong thuộc tính imports
+ * --> dựa vào import { NavbarComponent } from '../navbar/navbar.component'; để tìm ra file .ts
+ *
+ */
 
 @Component({
   selector: 'app-shell',
   standalone: true,
+  // Thêm các component vào đây
   imports: [Navbar, Sidebar],
   templateUrl: './shell.html',
   styleUrl: './shell.css',
